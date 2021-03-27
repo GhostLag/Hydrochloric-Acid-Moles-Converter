ce308922c53f66948f24db3a5004299e7749b1ee
GhostLag <61956567+GhostLag@users.noreply.github.com>
2021-03-28 10:53:32 +1100

    Fix spelling

--- hcl.cpp
+++ hcl.cpp
@@ -8,7 +8,7 @@ const double beaker = 60;
 int main() {
     double ratio1;
     double ratio2;
-    std::cout << "Type ratio of water to HCL\n";
+    std::cout << "Type ratio of water to HCl\n";
     std::cin >> ratio1;
     std::cin >> ratio2;
     std::cout << (ratio1 / (ratio1 + ratio2)) * (ratio2 / ratio1) << " concentration\n" << beaker / (waterMol * ratio1 + hydrochloricAcid * ratio2) * waterMol * ratio1 << " ml water\n" <<  beaker / (waterMol * ratio1 + hydrochloricAcid * ratio2) * hydrochloricAcid * ratio2 << " ml hydrochloric acid\n";
